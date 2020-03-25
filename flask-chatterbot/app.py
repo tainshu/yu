4b80c5f916b7bbe117d1edd4128dd50599790d47
zhanghu1 <2484756911@qq.com>
2020-03-25 22:41:58 +0800

    1

---
+++ flask-chatterbot/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, jsonify
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+app = Flask(__name__)
+
+english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+trainer = ChatterBotCorpusTrainer(english_bot)
+trainer.train("chatterbot.corpus.chinese")
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(english_bot.get_response(userText))
+
+
+@app.route("/api/chat/<text>")
+def get_bot_api(text):
+    res = str(english_bot.get_response(text))
+    return jsonify(res), 200
+
+
+if __name__ == "__main__":
+    app.run()
